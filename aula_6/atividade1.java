59ab8de5db4260d59bb6c6239bd2d10c7bb95e4f
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-03-23 21:07:50 -0300

    Crie a classe Veículo com o método mover() e, as subclasses: Carro e Bicicleta, cada uma imprimindo algodiferente.

---
+++ aula_6/atividade1.java
@@ -0,0 +1,35 @@
+// Crie a classe Veículo com o método mover() e, as subclasses: Carro e Bicicleta, cada uma imprimindo algodiferente.
+package aula_6;
+
+class Veiculo {
+    public void mover(){
+        System.out.println("O Veiculo esta se movendo");
+    }
+}
+
+class Carro extends Veiculo {
+    @Override
+    public void mover(){
+        System.out.println("O Carro está se movendo");
+    }
+}
+class Bicicleta extends Veiculo {
+    @Override
+    public void mover(){
+        System.out.println("A Bicicleta está se movendo");
+    }
+}
+
+
+public class atividade1 {
+    public static void main(String[] args) {
+        
+        Veiculo c1 = new Carro();
+        c1.mover();
+
+        Veiculo b1 = new Bicicleta();
+        b1.mover();
+
+    }
+}
+
