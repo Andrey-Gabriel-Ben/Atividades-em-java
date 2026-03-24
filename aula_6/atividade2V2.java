2aab713e5537e5a26ea7d9885a78759349a25640
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-03-23 21:49:49 -0300

    Crie a classe Forma com o método calcularArea() e, as subclasses: Quadrado e Circulo e, exibaas áreas correspondentes a cada figura

---
+++ aula_6/atividade2V2.java
@@ -0,0 +1,55 @@
+//Crie a classe Forma com o método calcularArea() e, as subclasses: Quadrado e Circulo e, exibaas áreas correspondentes a cada figura.
+package aula_6;
+
+class Forma {
+    void calcularArea(double area) {
+        System.out.println("A área da forma é " + area + " m²");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Quadrado extends Forma{
+
+    void calcularArea(double lado) {
+        double area;
+
+        area = lado*lado;
+
+        System.out.println("A área do quadrado de lado " + lado + "m é " + area + " m²");
+    }
+}
+
+class Circulo extends Forma{
+
+    void calcularArea(double raio ) {
+        double area;
+
+        area = Math.PI * (raio*raio);
+
+        System.out.println("A área do circulo de raio" + raio + "m é " + area + " m²");
+    }
+}
+
+public class atividade2V2 {
+    public static void main(String[] args) {
+        Quadrado q1 = new Quadrado();
+        q1.calcularArea(5);
+
+        Circulo c1 = new Circulo();
+        c1.calcularArea(5);
+    }
+}
