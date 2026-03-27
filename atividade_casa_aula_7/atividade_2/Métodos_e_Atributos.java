47b9e79f54a03605e6bb5e57d596bcaddddecc44
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-03-26 21:43:59 -0300

    para casa

---
+++ atividade_casa_aula_7/atividade_2/Métodos_e_Atributos.java
@@ -0,0 +1,28 @@
+package atividade_2;
+
+class ContaBancaria {
+
+}
+
+
+public class Métodos_e_Atributos {
+    String titualar;
+    double saldo;
+
+    public void setTitular(String nome){
+        this.titualar = nome;
+    }
+    public String getTitular() {
+        return titualar;
+    }
+    
+    public void setSaldo(double valor){
+        this.saldo = valor;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    //public void
+
+}
