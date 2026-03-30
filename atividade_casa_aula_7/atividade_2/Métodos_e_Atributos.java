e2cc55bcf07e31c5fbd961ba7d2a5a57e4b2cef3
Andrey Gabriel Ben <andreygabrielben@gmail.com>
2026-03-29 23:27:08 -0300

    Criar um sistema bancário capaz de exibir o saldo da conta na tela, verificar se há saldo disponível para saque e realizar depósitos.

--- atividade_casa_aula_7/atividade_2/Métodos_e_Atributos.java
+++ atividade_casa_aula_7/atividade_2/Métodos_e_Atributos.java
@@ -2,18 +2,15 @@ package atividade_2;
 
 class ContaBancaria {
 
-}
-
-
-public class Métodos_e_Atributos {
-    String titualar;
+    String titular;
     double saldo;
 
+    //sets e gets ini
     public void setTitular(String nome){
-        this.titualar = nome;
+        this.titular = nome;
     }
     public String getTitular() {
-        return titualar;
+        return titular;
     }
     
     public void setSaldo(double valor){
@@ -22,7 +19,42 @@ public class Métodos_e_Atributos {
     public double getSaldo() {
         return saldo;
     }
+    //sets e gets end
 
-    //public void
+    public void depositar(double valor) {
+        saldo += valor;
+    }
 
+    public void sacar(double valor){
+
+        if (saldo < valor){
+            System.out.println("Saldo insuficiente");
+        } else {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+        }
+
+    }
+
+    public void exibirSaldo(){
+        System.out.println("O saldo atual é de " + saldo);
+    }
+}
+
+public class Métodos_e_Atributos {
+    public static void main(String[] args) {
+        
+        ContaBancaria c1 = new ContaBancaria();
+        c1.setTitular("Bruno");
+        c1.setSaldo(3500);
+
+        c1.exibirSaldo();
+
+        c1.depositar(1500);
+
+        c1.sacar(6000);
+        c1.sacar(5000);
+
+        c1.exibirSaldo();
+    }
 }
